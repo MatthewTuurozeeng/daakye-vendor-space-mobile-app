8463cd945923eb018494770f3041cdc1091ce83e
Matthew Tuurozeeng <tuurozeeng.matthew@ashesi.edu.gh>
2026-05-15 14:22:39 +0000

    implement text input to add more shopping by receiving text from keyboard

--- daakye-vendor-space-app/app/index.tsx
+++ daakye-vendor-space-app/app/index.tsx
@@ -1,14 +1,59 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput} from "react-native";
 import { theme } from "../theme";
 import { ShoppingList } from "../components/ShoppingList";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  {
+    id: "1",
+    name: "Coffee",
+  },
+  {
+    id: "2",
+    name: "Bread",
+  },
+  {
+    id: "3",
+    name: "Tea",
+  },
+  {
+    id: "4",
+    name: "Ice",
+  },
+];
+
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const [shoppingList, setshoppingList] = useState<ShoppingListItemType[]>(initialList);
+  const handleSubmit = () => {
+    if (value){
+      const newShoppingList: ShoppingListItemType[] = [
+        {id: new Date().toTimeString(), name: value},
+        ...shoppingList,
+      ];
+      setshoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+  
   return (
     <View style={styles.container}>
-      <ShoppingList name="Coffee" isComleted/>
-      <ShoppingList name="Bread" />
-      <ShoppingList name="Tea" isComleted/>
-      <ShoppingList name="Ice" />
+      <TextInput placeholder="e.g Coffee" style= {styles.textInput}
+        value={value}
+        onChangeText={setValue}
+        keyboardType="email-address"
+        onSubmitEditing={handleSubmit}
+  
+      />
+      {shoppingList.map((item) => (
+        <ShoppingList key={item.id} name={item.name} />
+      ))}
     </View>
   );
 }
@@ -18,7 +63,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    paddingTop: 200,
+    paddingTop: 12,
     paddingBottom: 16,
     paddingHorizontal: 12,
     gap: 16,
@@ -26,6 +71,15 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: "800",
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: theme.colorLightGray,
+    borderRadius: 50,
+    padding: 12,
+    marginBottom: 12,
+    marginHorizontal: 12,
+    fontSize: 18,
   },
 });
 
